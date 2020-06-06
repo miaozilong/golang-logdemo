7468a3606d3ec0621c7f2350e0b8fdb4a0c87ef9
miaozilong <miao.zilong@qq.com>
2020-06-06 23:58:52 +0800

    首次提交

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "logdemo/utils"
+
+func main() {
+	utils.Debug("hello world")
+}
---
+++ utils/main.go
@@ -0,0 +1,10 @@
+package utils
+
+import log "github.com/cihub/seelog"
+
+func Debug(v ...interface{}) {
+	logger, _ := log.LoggerFromConfigAsFile("conf/seelog.xml")
+	defer log.Flush()
+	log.ReplaceLogger(logger)
+	log.Debug(v)
+}
